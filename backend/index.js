d1c289dd574f48e837eb6a25c9db9194ba2d4e16
abdullah <abmunawer@gmail.com>
2022-10-20 23:56:25 +0500

    routes & schema for storing notes and users

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,12 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/auth', require('./routes/notes'))
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
